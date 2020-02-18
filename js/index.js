af02480ad18d0076b3f56e3877df374097b3afa5
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-02-17 16:35:06 -0800

    Trying to figure this out. No changes came up.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 // Your code goes here
 let halt = document.querySelectorAll('.nav-link');
 halt.forEach(navLink => {
-    navLink.addEventListener('click', (preDef) => {
-        preDef.preventDefault();
+    navLink.addEventListener('click', () => {
+        halt.preventDefault();
     })
 });
 
 let buttons = document.querySelectorAll('.btn');
-
+buttons.forEach(btnEvent => {
+    btnEvent.addEventListener('click', () => {
+        buttons.style.color = 'red';
+        
+    }
+    
+    )
+})
