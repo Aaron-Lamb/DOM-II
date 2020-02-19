317a398311f15ca42669fc706f0c689e6074878d
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-02-18 17:18:03 -0800

    Added one click event to buttons[0] that has an if statement, a mouse enter and leave for both buttons 1, and 2, and made two variables, one for the header, and one for the footer.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,41 @@
 // Your code goes here
-let halt = document.querySelectorAll('.nav-link');
+let halt = document.querySelectorAll('a.nav-link');
 halt.forEach(navLink => {
     navLink.addEventListener('click', () => {
         halt.preventDefault();
     })
-});
-
-let buttons = document.querySelectorAll('.btn');
-buttons.forEach(btnEvent => {
-    btnEvent.addEventListener('click', () => {
-        buttons.style.color = 'red';
-        
-    }
-    
-    )
 })
+
+let mainContent = document.querySelector('body');
+let headBar = document.querySelector('header');
+let footBar = document.querySelector('footer');
+
+let buttons = document.querySelectorAll('div.btn');
+    buttons[0].addEventListener('click', (buttoner) => {
+        if (mainContent.style.backgroundColor === 'aliceblue') {
+            mainContent.style.backgroundColor = 'lightblue';
+          } else {
+            mainContent.style.backgroundColor = 'aliceblue';
+          }
+          buttoner.stopPropagation();
+    })
+    
+    buttons[1].addEventListener('mouseenter', (mover) => {
+        mainContent.style.color = 'darkblue';
+        mover.stopPropagation();
+    })
+
+    buttons[1].addEventListener('mouseleave', (bye) => {
+        headBar.style.color = 'red';
+        bye.stopPropagation();
+    })
+
+    buttons[2].addEventListener('mouseleave', (crazy) => {
+        headBar.style.backgroundColor = 'pink';
+        crazy.stopPropagation();
+    })
+
+    buttons[2].addEventListener('mouseenter', (hello) => {
+        footBar.style.backgroundColor = 'lavender';
+        hello.stopPropagation();
+    })
