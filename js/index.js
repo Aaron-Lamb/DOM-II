4e7ad3441969d363237556f22b58774c1940cc67
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-09 21:33:29 -0700

    Pluggin along

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 // Your code goes here
+let body = document.querySelector("body");
+body.addEventListener('contextmenu', (event) => {
+    event.target.style.backgroundColor = "black";
+    event.target.style.color = "cyan";
+})
+body.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = "white";
+    event.target.style.color = "black";
+})
+
 let busImg = document.querySelectorAll("img")[0];
 busImg.addEventListener('dblclick', (event) => {
     event.stopPropagation();
     event.target.style.border = "3px solid blue";
     event.target.style.padding = "2%";
 })
-
-let imgTwo = document.querySelectorAll("img")[1];
-imgTwo.addEventListener('keydown', (event => {
-    event.target.style.transform = "rotate(720deg)";
-    event.target.style.transition = "transform 2s";
-}))
 
 let navigation = document.querySelectorAll("header")[0];
 navigation.addEventListener('mouseenter', (event) => {
