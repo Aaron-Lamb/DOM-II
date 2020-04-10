aa15cc3058399b196f72772738e3fb11c2c308e8
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-09 21:27:41 -0700

    Still working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+let busImg = document.querySelectorAll("img")[0];
+busImg.addEventListener('dblclick', (event) => {
+    event.stopPropagation();
+    event.target.style.border = "3px solid blue";
+    event.target.style.padding = "2%";
+})
+
+let imgTwo = document.querySelectorAll("img")[1];
+imgTwo.addEventListener('keydown', (event => {
+    event.target.style.transform = "rotate(720deg)";
+    event.target.style.transition = "transform 2s";
+}))
+
+let navigation = document.querySelectorAll("header")[0];
+navigation.addEventListener('mouseenter', (event) => {
+    event.target.style.backgroundColor = "blanchedalmond";
+})
+navigation.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = 'cyan';
+})
+
+let heading = document.querySelectorAll("header")[1];
+console.log(heading);
+heading.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = "slategray";
+})
+
+let anchors = document.querySelectorAll("a");
+anchors.forEach((anchor) => {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+})
